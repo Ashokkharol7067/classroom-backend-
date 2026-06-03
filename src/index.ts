01a0593093804cf87262601ca1655025c5dd78d7
Ashok Kharol <ashokkharol8959@gmail.com>
2026-06-03 20:50:57 +0530

    implement GET all subjects routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import subjectsRouter from "./routes/subjects.js";
+import cors from 'cors'
 
 const app = express();
 const PORT = 8000;
 
+app.use(cors({
+	origin: process.env.FRONREND_URL,
+	methods: ['GET', 'POST', 'PUT', 'DELETE'],
+	credentials: true
+}))
+
 app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter)
+
 
 app.get("/", (req, res) => {
 	res.json({ message: "Classroom backend is running" });
